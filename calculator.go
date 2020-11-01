9fef060516a3ac6171e161e3d76fb3c5ffb238b8
Vedran Krivokuca <vedran@krivokuca.net>
2020-11-01 17:29:48 +0100

    Down to the testCases

--- calculator.go
+++ calculator.go
@@ -3,11 +3,17 @@ package calculator
 
 // Add takes two numbers and returns the result of adding them together.
 func Add(a, b float64) float64 {
-return a + b
+	return a + b
 }
 
 // Subtract takes two numbers and returns the result of subtracting the second
 // from the first.
 func Subtract(a, b float64) float64 {
-	return b - a
+	return a - b
+}
+
+// Multipilies two numbers and returns the result of multiplying the second
+// against the first
+func Multiply(a, b float64) float64 {
+	return a * b
 }
